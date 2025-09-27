8f2febcdd3a47c028b4e35e5478cd4bf89c30341
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2025-09-27 13:34:10 -0500

    feat: send location risk to queue and cache most recent for all locations

--- weather-risk/live_updater.py
+++ weather-risk/live_updater.py
@@ -1,22 +1,45 @@
 import time
+import json
 from .ingest.weather_location import WeatherLocation
+from .services.queue import push_to_queue
+from .services.cache import cache_latest_risk
 
 LOCATIONS = [
   {"lat": 29.721345, "lon": -95.342013},
   {"lat": 29.758828, "lon": -95.370804}
 ]
 
-def run_live_update():
+
+def live_update_generator():
   while True:
-    print(f"--- Update at {time.strftime('%Y-%m-%d %H:%M:%S')} ---")
-    
+    payload = f"--- Update at {time.strftime('%Y-%m-%d %H:%M:%S')} ---"
     for loc in LOCATIONS:
       weather = WeatherLocation(loc["lat"], loc["lon"])
       risk = weather.get_rain_risk()
-      print(f"Location {loc['lat']},{loc['lon']}:\n{risk}\n")
+      payload += f"\nLocation {loc['lat']},{loc['lon']}:\n{risk}\n"
+      timestamp = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+      cache_latest_risk(loc["lat"], loc["lon"], risk, timestamp)
 
-    print("Waiting for next check...\n\n")
-    time.sleep(5)
+      event = {
+        "lat": loc["lat"],
+        "lon": loc["lon"],
+        "risk": risk,
+        "timestamp": timestamp
+      }
+      push_to_queue(json.dumps(event))
+
+    yield payload
+    time.sleep(20)
+
+def get_latest_payload():
+  payload = f"--- Update at {time.strftime('%Y-%m-%d %H:%M:%S')} ---"
+  for loc in LOCATIONS:
+    weather = WeatherLocation(loc["lat"], loc["lon"])
+    risk = weather.get_rain_risk()
+    payload += f"\nLocation {loc['lat']},{loc['lon']}:\n{risk}\n"
+  return payload
 
 if __name__ == "__main__":
-  run_live_update()
+  for payload in live_update_generator():
+    print(payload)
+    print("Waiting for next check...\n\n")
